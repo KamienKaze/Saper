a26cc5e738b5aaccc26485d603649f6f6ea18572
KamienKaze <HygrekRazuuk@gmail.com>
2022-11-15 01:20:00 +0100

    refactor: almost whole script refactored

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+// Search for elements
+const board = document.getElementById("game_board");
+
 // Declare constant values
 const board_size = 10;
 const cell_size = 80 / board_size;
 const minesCount = 15
-
-// Search for elements
-const board = document.getElementById("game_board");
 
 // Declare variables
 var virtual_board = [];
@@ -16,53 +16,42 @@ addEventListener("load", onInit);
 // Execute on page initialization
 function onInit () {
 
-    // Set grid template dynamically
-    board.style.gridTemplateColumns = "repeat(" + board_size + ", " + cell_size + "vmin)";
-    board.style.gridTemplateRows = "repeat(" + board_size + ", " + cell_size + "vmin)";
+    createVirtualBoard(board_size);
+    drawBoard(virtual_board);
+
+    console.log(virtual_board);
+}
+
+// ------------------------------ VIRTUAL BOARD ------------------------------
+
+function createVirtualBoard(board_size) {
 
     // Declare variables
     var virtual_board_in = [];
-    var column_id = 0;
-    var row_id;
 
     // Create all cells and array
     for(i = 0; i < board_size; i++) {
-
-        row_id = 0
-
         for(j = 0; j < board_size; j++) {
 
-            var cell = document.createElement('div');    
-
-            var id = 'cell_'  + column_id + "_" + row_id;
-    
-            cell.className = "cell";
-            cell.setAttribute('id', id);
-
-            board.appendChild(cell);
             virtual_board_in.push(0);
-
-            row_id++;
 
         }
 
         virtual_board.push(virtual_board_in);
         virtual_board_in = [];
 
-        column_id++;
-
-    }
-
-    for(i = 0; i < board_size; i++) {
-        for(j = 0; j < board_size; j++) {
-            virtual_board[i][j] = countMines(i, j);
-        }
     }
 
     assignMinesOnBoard(minesCount);
 
+    for(k = 0; k < board_size; k++) {
+        for(l = 0; l < board_size; l++) {
+            if(virtual_board[k][l] != -1){
+                virtual_board[k][l] = countMines(k, l);
+            }
+        }
+    }
 
-    console.log(virtual_board);
 }
 
 // Put mines in appropriate places
@@ -89,21 +78,59 @@ function canPutTheMine(x, y) {
     }
 }
 
+// Count all mines in surrounding
 function countMines(x, y) {
 
-    var minesInSurroundings = 0;
+    minesInSurrounding = 0;
 
-    for(a = x - 1; a <= x + 1; a++) {
-        if(a < 0 || a > board_size) { continue; }
-        for(b = y - 1; b <= b + 1; b++) {
-            if(b < 0 || b > board_size) { continue; }
-            if(virtual_board[a][b] != -1) { continue; }
+    for(i = x - 1; i <= x + 1; i++) {
 
-            console.log(minesInSurroundings);
-            minesInSurroundings++;
+        if(i < 0 || i >= board_size){ continue; }
+
+        for(j = y - 1; j <= y + 1; j++) {
+
+            if(j < 0 || j >= board_size){ continue; }
+
+            if(virtual_board[i][j] == -1) { minesInSurrounding++; }
         }
-        
     }
 
-    return minesInSurroundings;
+    return minesInSurrounding;
+}
+
+// ------------------------------ PHYSICAL BOARD ------------------------------
+
+function drawBoard(virtual_board) {
+
+    // Set grid template dynamically
+    board.style.gridTemplateColumns = "repeat(" + board_size + ", " + cell_size + "vmin)";
+    board.style.gridTemplateRows = "repeat(" + board_size + ", " + cell_size + "vmin)";
+
+    var column_id = 0;
+    var row_id;
+
+    // Create cells
+    for(i = 0; i < board_size; i++) {
+
+        row_id = 0
+
+        for(j = 0; j < board_size; j++) {
+
+            var cell = document.createElement('div');    
+
+            var id = 'cell_'  + column_id + "_" + row_id;
+    
+            cell.className = "cell notChecked";
+            cell.setAttribute('id', id);
+            cell.innerHTML = virtual_board[i][j];
+
+            board.appendChild(cell);
+
+            row_id++;
+
+        }
+
+        column_id++;
+
+    }
 }
